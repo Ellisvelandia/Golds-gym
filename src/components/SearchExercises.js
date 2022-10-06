33ca155586c9871846f8aabc78b6e486e049109c
ellisvelandia <eyis619@gmail.com>
2022-10-05 23:29:41 -0500

    gym

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,9 +4,8 @@ import { Box, Stack, Typography, Button, TextField } from "@mui/material";
 import { fetchData, exerciseOptions } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
@@ -84,7 +83,12 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-        <HorizontalScrollbar data={bodyParts} bodyParts />
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyParts
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
